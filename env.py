e4b525477fa9e164790f84f22e0175d426b9e08a
Coolafdood <chazeldred55@gmail.com>
2025-10-01 15:24:37 +0100

    deployment settings

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ.setdefault(
+    "DATABASE_URL",
+    "postgresql://neondb_owner:npg_dl0CytY9Mpna@ep-delicate-unit-ag1rq77b."
+    "c-2.eu-central-1.aws.neon.tech/plow_spoon_spree_113241"
+)
